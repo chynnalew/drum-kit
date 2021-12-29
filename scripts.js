fd7f171707454dfe764a528e212dab4ddaa20c53
chynnalew <chynnalew@yahoo.com>
2021-12-28 18:57:16 -0800

    add audio play, add return if no audio element

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function (event) {
   const audio = document.querySelector(`audio[data-key='${event.keyCode}'`);
-  console.log(audio)
+  if (!audio) {
+    return;
+  }
+  
+  audio.play();
 } )
