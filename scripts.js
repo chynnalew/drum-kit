17b69052e827ffe73fb3538bc5acc3648d256e11
chynnalew <chynnalew@yahoo.com>
2021-12-28 22:42:39 -0800

    add small media styling

--- scripts.js
+++ scripts.js
@@ -1,17 +1,28 @@
-window.addEventListener('keydown', function (event) {
+window.addEventListener("keydown", function (event) {
   const audio = document.querySelector(`audio[data-key='${event.keyCode}'`);
   if (!audio) {
     return;
   }
   audio.currentTime = 0;
   audio.play();
-})
+});
 
-function clicked (key) {
+function clicked(key) {
   const audio = document.querySelector(`audio[data-key='${key}'`);
   if (!audio) {
     return;
   }
   audio.currentTime = 0;
   audio.play();
+}
+
+function clickI() {
+  document.getElementById("instructions").style.display = 'block';
+  document.getElementById("drumKit").style.display = 'none';
+}
+
+function clickDK() {
+  document.getElementById("instructions").style.display = 'none';
+  document.getElementById("drumKit").style.display = 'block';
+  console.log('clicked dk');
 }
